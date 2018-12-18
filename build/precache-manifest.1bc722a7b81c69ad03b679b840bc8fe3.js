1c50b7cd945281fb51093b95775721cc3ae12b8f
Bojan Milicev <bojan.milicev@homea.io>
2018-12-18 09:32:27 +0100

    Deploy for gh-page

---
+++ build/precache-manifest.1bc722a7b81c69ad03b679b840bc8fe3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "07737735cbfcf9b925f67a1d014a456f",
+    "url": "/laziR/static/media/up-arrow.07737735.svg"
+  },
+  {
+    "revision": "1e4930e19d5d5795f35ff04571cfad12",
+    "url": "/laziR/static/media/lazi.1e4930e1.png"
+  },
+  {
+    "revision": "05b357811dfff22c6747",
+    "url": "/laziR/static/js/runtime~main.05b35781.js"
+  },
+  {
+    "revision": "0c79bc30354c87c4bc7f",
+    "url": "/laziR/static/js/main.0c79bc30.chunk.js"
+  },
+  {
+    "revision": "aa4ec6e8dd8b9f63bc33",
+    "url": "/laziR/static/js/1.aa4ec6e8.chunk.js"
+  },
+  {
+    "revision": "0c79bc30354c87c4bc7f",
+    "url": "/laziR/static/css/main.1b1c8df8.chunk.css"
+  },
+  {
+    "revision": "8cdca37a5ee18218817f186e27e89647",
+    "url": "/laziR/index.html"
+  }
+];
