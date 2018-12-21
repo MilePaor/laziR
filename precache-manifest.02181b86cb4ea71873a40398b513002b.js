9f669da3c7c2f6124abfc9d58fc273a100ce196c
Bojan Milicev <bojan.milicev@homea.io>
2018-12-21 11:32:25 +0100

    Updates

---
+++ precache-manifest.02181b86cb4ea71873a40398b513002b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e4930e19d5d5795f35ff04571cfad12",
+    "url": "/laziR/static/media/logo.1e4930e1.png"
+  },
+  {
+    "revision": "946fb8bb4f6e56d421e92515d74b0985",
+    "url": "/laziR/static/media/cloud.946fb8bb.svg"
+  },
+  {
+    "revision": "05b357811dfff22c6747",
+    "url": "/laziR/static/js/runtime~main.05b35781.js"
+  },
+  {
+    "revision": "cfeb2c0e937f842cc31b",
+    "url": "/laziR/static/js/main.cfeb2c0e.chunk.js"
+  },
+  {
+    "revision": "d06299bebf0232bf2e81",
+    "url": "/laziR/static/js/1.d06299be.chunk.js"
+  },
+  {
+    "revision": "cfeb2c0e937f842cc31b",
+    "url": "/laziR/static/css/main.e85ff512.chunk.css"
+  },
+  {
+    "revision": "149b74a46a2e8e901deaf2af0b7b3b27",
+    "url": "/laziR/index.html"
+  }
+];
