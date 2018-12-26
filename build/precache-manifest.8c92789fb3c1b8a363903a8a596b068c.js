14906cbdec0bac81a38305381ced33b9f528eb86
MilePaor <zlinindza@gmail.com>
2018-12-26 21:17:45 +0100

    Deploy final

---
+++ build/precache-manifest.8c92789fb3c1b8a363903a8a596b068c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e4930e19d5d5795f35ff04571cfad12",
+    "url": "/lazi-react/static/media/logo.1e4930e1.png"
+  },
+  {
+    "revision": "a410f1ea0711fd90594893626edd4533",
+    "url": "/lazi-react/static/media/cloud.a410f1ea.svg"
+  },
+  {
+    "revision": "0e897afbb5d0a11e0aca",
+    "url": "/lazi-react/static/js/runtime~main.0e897afb.js"
+  },
+  {
+    "revision": "3fc280abf7da6999c8c4",
+    "url": "/lazi-react/static/js/main.3fc280ab.chunk.js"
+  },
+  {
+    "revision": "ca0bd9f406def28a684f",
+    "url": "/lazi-react/static/js/1.ca0bd9f4.chunk.js"
+  },
+  {
+    "revision": "3fc280abf7da6999c8c4",
+    "url": "/lazi-react/static/css/main.ae72001c.chunk.css"
+  },
+  {
+    "revision": "134fe097164ea6ef7b8a4ca64b65da3e",
+    "url": "/lazi-react/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/laziR/precache-manifest.77a809b3be2b87171da29a4e0114ac73.js"
+  "/lazi-react/precache-manifest.8c92789fb3c1b8a363903a8a596b068c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/laziR/index.html", {
+workbox.routing.registerNavigationRoute("/lazi-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
